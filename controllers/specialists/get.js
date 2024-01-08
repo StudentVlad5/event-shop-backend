2e20a4e4e9d49fbb9706501e5685c7592f92aff3
JuliaGolban <julia.j.shcherban@gmail.com>
2024-01-08 17:56:31 +0200

    update get specialists

--- controllers/specialists/get.js
+++ controllers/specialists/get.js
@@ -1,9 +1,24 @@
-const { ValidationError } = require("../../helpers");
-const { Specialists } = require("../../models");
+const { ValidationError } = require('../../helpers');
+const { Specialists } = require('../../models');
 
 const get = async (req, res, next) => {
+  const { sort } = req.query;
+  let specialists = [];
+
   try {
-    const specialists = await Specialists.find();
+    if (sort) {
+      if (sort === 'rating')
+        specialists = await Specialists.find().sort({
+          rating: -1,
+        });
+    } else {
+      const specialists = await Specialists.find().sort({
+        nameUa: 1,
+        nameFr: 1,
+        nameRu: 1,
+      });
+    }
+
     res.status(200).json(specialists);
   } catch (err) {
     throw new ValidationError(err.message);
