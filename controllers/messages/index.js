f79c0074cd3d6d995120146bb00bf3c9de7de570
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-11 11:42:42 +0200

    add CRUD for messages

--- controllers/messages/index.js
+++ controllers/messages/index.js
@@ -2,10 +2,12 @@ const createMessage = require('./createMessage');
 const getMessages = require('./getMessages');
 const getMessageById = require('./getMessageById');
 const deleteMessage = require('./deleteMessage');
+const updateMessage = require('./updateMessages');
 
 module.exports = {
   createMessage,
   getMessages,
   getMessageById,
   deleteMessage,
+  updateMessage,
 };
