09017e04be3399c41b7461bfa9bbe4362ddf11d0
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-14 15:16:57 +0200

    add create orders

--- controllers/orders/createOrder.js
+++ controllers/orders/createOrder.js
@@ -1,5 +1,6 @@
 const { ValidationError } = require("../../helpers");
 const { Orders } = require("../../models");
+const { ActiveEvents } = require("../../models");
 
 const createOrder = async (req, res, next) => {
   const {
@@ -25,9 +26,20 @@ const createOrder = async (req, res, next) => {
     priceTotal,
     status: status ? status :"new",
   };
+
+
+  const updatedD = await ActiveEvents.find({article_eventID:activeEventID});
+  const updatedData = updatedD[0];
+  if(updatedData.booking) {updatedData.booking = updatedData.booking + newData.bookingSeats};
+  if (updatedData.vacancies){updatedData.vacancies = updatedData.vacancies - newData.bookingSeats};
+  if (updatedData.vacancies < 0 ){updatedData.vacancies = 0};
+
   console.log("CREATE OREDER:", newData);
   try {
     const resCreate = await Orders.create(newData);
+    if(resCreate){ await ActiveEvents.findOneAndUpdate({ article_eventID: activeEventID }, updatedData, {
+      new: true,
+    })};
     return res.status(201).json(resCreate);
   } catch (err) {
     throw new ValidationError(err.message);
