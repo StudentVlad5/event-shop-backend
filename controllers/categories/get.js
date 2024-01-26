d671a4417db61c9c7c312bbd9ad3e0223ef2c847
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-26 09:56:58 +0200

    add other in sort category

--- controllers/categories/get.js
+++ controllers/categories/get.js
@@ -3,7 +3,7 @@ const { Categories } = require("../../models");
 
 const get = async (req, res, next) => {
   try {
-    const categories = await Categories.find();
+    const categories = await Categories.find().sort({ createdAt: -1 });
     res.status(200).json(categories);
   } catch (err) {
     throw new ValidationError(err.message);
